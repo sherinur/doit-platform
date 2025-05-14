fefe88e1401fb0440082a063f027a24b7bdb6ceb
Yernar Bukemabay <yernarbukembay@gmail.com>
2025-05-14 22:40:55 +0500

    delete docker-compose

--- user-service/internal/adapter/controller/grpc/server/options.go
+++ user-service/internal/adapter/controller/grpc/server/options.go
@@ -8,19 +8,6 @@ import (
 	"google.golang.org/grpc/keepalive"
 )
 
-// func (a *API) setOptions(ctx context.Context, secretket string) []grpc.ServerOption {
-// 	opts := []grpc.ServerOption{
-// 		grpc.KeepaliveParams(keepalive.ServerParameters{
-// 			MaxConnectionAge:      a.cfg.MaxConnectionAge,
-// 			MaxConnectionAgeGrace: a.cfg.MaxConnectionAgeGrace,
-// 		}),
-// 		grpc.UnaryInterceptor(interceptor.AuthInterceptor(secretket)),
-// 		grpc.MaxRecvMsgSize(a.cfg.MaxRecvMsgSizeMiB * (1024 * 1024)), // MaxRecvSize * 1 MB
-// 	}
-
-// 	return opts
-// }
-
 func (a *API) setOptions(ctx context.Context, secretkey string) []grpc.ServerOption {
 	opts := []grpc.ServerOption{
 		// Params
